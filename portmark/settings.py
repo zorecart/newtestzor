a21276c82912cc0bebf096f1e17f39e68173ce21
zorecart <zorevinacart@gmail.com>
2025-05-09 03:27:06 +0100

    Update settings.py

--- portmark/settings.py
+++ portmark/settings.py
@@ -11,7 +11,10 @@ from django.contrib.messages import constants as messages
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["web-production-f4d8.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-f4d8.up.railway.app",
+    "https://www.zorevinacart.store"
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
